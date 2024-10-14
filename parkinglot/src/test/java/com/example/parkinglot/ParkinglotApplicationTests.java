880aa8295c4fa7dc4f356433dd7b448feed18d4d
Ashley Huang <107271362+yhuang-it@users.noreply.github.com>
2024-10-14 11:29:11 -0400

    Add files via upload
    spring template structure

---
+++ parkinglot/src/test/java/com/example/parkinglot/ParkinglotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
