6dcae36636d9434f411de525751c3db7339d308f
work <shuwana.brown@peopleshores.com>
2024-10-16 11:54:36 -0400

    Structured the sidebar and content side

--- src/main/webapp/the-lot/src/app/app.component.ts
+++ src/main/webapp/the-lot/src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import { AdminSidebarComponent } from "./admin-sidebar/admin-sidebar.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+    AdminSidebarComponent,
+    DashboardComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
